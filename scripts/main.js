a5f278bec52b28f44b4dbae25391e707b0fed8ad
Renato Porfirio <95599633+RenatoPorfirio@users.noreply.github.com>
2024-03-05 12:14:23 -0300

    alterações de compatibilidade e estilização da tabela

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ async function buscarPalavraAleatoria(){
     return resposta;
 }
 
-var palavraAleatoria;
+var cola;
 var entradas = [];
 
 let botaoReiniciar = document.querySelector(".botao-circulo-header");
@@ -21,43 +21,8 @@ botaoReiniciar.onclick = () => {
     window.location.reload();
 }
 
-botaoAvaliar.onclick = () => {
-    if(entradas.every((entrada) => entrada.value)){
-        let str = entradas.reduce((acumulador, entrada) => acumulador + entrada.value, "");
-        let palavraOculta = palavraAleatoria.normalize('NFD').replace(/[^a-zA-Z\s]/g, "");
-        if(str.toLowerCase() == palavraOculta){
-            alert("Parabéns, você acertou!");
-            window.location.reload();
-        }
-        else{
-            let linha = document.createElement('tr');
-            linha.setAttribute('class', 'linha-tabela');
-            for(let i = 0; i < entradas.length; i++){
-                let item = document.createElement('td');
-                item.innerHTML = entradas[i].value;
-                if(entradas[i].value == palavraOculta[i]){
-                    item.setAttribute('class', 'item-tabela correto');
-                }
-                else{
-                    item.setAttribute('class', 'item-tabela errado');
-                }
-                linha.appendChild(item);
-            }
-            tabelaTentativas.appendChild(linha);
-
-            entradas = entradas.map((entrada) => {
-                entrada.value = "";
-                return entrada;
-            });
-        }
-    }
-    else{
-        alert("Preencha todos os campos!");
-    }
-    entradas[0].focus();
-}
-
 async function carregarPalavraAleatoria(){
+    let palavraAleatoria;
     do{
         palavraAleatoria = await buscarPalavraAleatoria();
     } while(palavraAleatoria.length > 7);
@@ -65,7 +30,54 @@ async function carregarPalavraAleatoria(){
 }
 
 async function iniciarJogo(){
-    await carregarPalavraAleatoria();
+    let palavraAleatoria = await carregarPalavraAleatoria();
+
+    cola = palavraAleatoria;
+
+    botaoAvaliar.onclick = () => {
+        if(entradas.every((entrada) => entrada.value)){
+            let str = entradas.reduce((acumulador, entrada) => acumulador + entrada.value, "");
+            let palavraOculta = palavraAleatoria.normalize('NFD').replace(/[^a-zA-Z\s]/g, "");
+            //let existencia = await fetch('https://api.dicionario-aberto.net/word'+palavraOculta);
+            // if(existencia.length === 0){
+            //     alert('Digite uma palavra válida!!');
+            // }
+            //else
+             if(str.toLowerCase() == palavraOculta){
+                alert("Parabéns, você acertou!");
+                window.location.reload();
+            }
+            else{
+                let linha = document.createElement('tr');
+                linha.setAttribute('class', 'linha-tabela');
+                for(let i = 0; i < entradas.length; i++){
+                    let item = document.createElement('td');
+                    item.innerHTML = entradas[i].value;
+                    if(entradas[i].value == palavraOculta[i]){
+                        item.setAttribute('class', 'item-tabela correto');
+                    }
+                    else{
+                        item.setAttribute('class', 'item-tabela errado');
+                    }
+                    linha.appendChild(item);
+                }
+                tabelaTentativas.appendChild(linha);
+    
+                entradas = entradas.map((entrada) => {
+                    entrada.value = "";
+                    return entrada;
+                });
+            }
+        }
+        else{
+            alert("Preencha todos os campos!");
+        }
+        entradas[0].focus();
+    }
+
+    const [letras, tentativas] = document.getElementsByClassName('titulo-conteudo-principal');
+    letras.innerText += palavraAleatoria.length + ' LETRAS';
+    tentativas.innerText += ' ' + 10;
 
     for(let i = 0; i < palavraAleatoria.length; i++){
         let entrada = document.createElement('input');
@@ -74,43 +86,24 @@ async function iniciarJogo(){
         entradas.push(entrada);
         camposEntrada.appendChild(entrada);
     }
-    for(let i = 1; i < palavraAleatoria.length - 1; i++){
-        entradas[i].addEventListener('input', () => {
-            switch(entradas[i].textLength){
-                case 0:
-                    entradas[i-1].focus();
-                    break;
-                case 1:
-                    entradas[i+1].focus();
-                    break;
-                default:
+    for(let i = 0; i < palavraAleatoria.length; i++){
+        entradas[i].addEventListener('input', (e) => {
+            if(e.inputType === 'deleteContentBackward' && i != 0){
+                entradas[i-1].focus();
+            }
+            else if(e.inputType === 'insertText'){
+                if(entradas[i].value.length > 1){
                     entradas[i].value = entradas[i].value[0];
+                }
+                else if(i == palavraAleatoria.length - 1){
+                    botaoAvaliar.focus();
+                }
+                else{
+                    entradas[i+1].focus();
+                }
             }
         });
     }
-    entradas[0].addEventListener('input', () => {
-        switch(entradas[0].textLength){
-            case 0:
-                break;
-            case 1:
-                entradas[1].focus();
-                break;
-            default:
-                entradas[0].value = entradas[0].value[0];
-        }
-    });
-    entradas[palavraAleatoria.length - 1].addEventListener('input', () => {
-        switch(entradas[palavraAleatoria.length - 1].textLength){
-            case 0:
-                entradas[palavraAleatoria.length - 2].focus();
-                break;
-            case 1:
-                botaoAvaliar.focus();
-                break;
-            default:
-                entradas[palavraAleatoria.length - 1].value = entradas[palavraAleatoria.length - 1].value[0];
-        }
-    });
 }
 
 iniciarJogo();
